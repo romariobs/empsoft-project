dc0003c592b57823a80617a4bbe66695dd32f744
Stenio Elson <stenio.araujo@ccc.ufcg.edu.br>
2017-04-02 14:21:54 -0300

    Carregar dados no splash

--- app/src/main/java/br/edu/ufcg/empsoft/activities/Splash.java
+++ app/src/main/java/br/edu/ufcg/empsoft/activities/Splash.java
@@ -5,12 +5,18 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.List;
+
 import br.edu.ufcg.empsoft.MainActivity;
 import br.edu.ufcg.empsoft.R;
+import br.edu.ufcg.empsoft.models.CallBack;
+import br.edu.ufcg.empsoft.models.Database;
+import br.edu.ufcg.empsoft.models.Fazenda;
 
 public class Splash extends AppCompatActivity {
 
-    private final int SPLASH_DISPLAY_LENGTH = 2000;
+    private final int SPLASH_DISPLAY_LENGTH = 500;
+    private Database database = Database.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +26,13 @@ public class Splash extends AppCompatActivity {
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
-                /* Create an Intent that will start the Menu-Activity. */
-                Intent mainIntent = new Intent(Splash.this, MainActivity.class);
-                Splash.this.startActivity(mainIntent);
-                Splash.this.finish();
+                database.addListener(new CallBack<List<Fazenda>>() {
+                    public void onDataChange(List<Fazenda> result) {
+                        Intent mainIntent = new Intent(Splash.this, MainActivity.class);
+                        Splash.this.startActivity(mainIntent);
+                        Splash.this.finish();
+                    }
+                });
             }
         }, SPLASH_DISPLAY_LENGTH);
     }
